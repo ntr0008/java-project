499c3c47b8efc870eda6f21f4d2fa3f298cf7826
ntr0008 <NTR@adminisatorsmbp.mynetworksettings.com>
2023-08-10 09:58:28 -0400

    changes made to candles

--- Main.java
+++ Main.java
@@ -23,16 +23,16 @@ class Candle {
 public class Main {
     public static void main(String[] args) {
         // Creating 3 different types of candles
-        Candle candle1 = new Candle("Luscious Lavender", 3, 5, 7.50);
-        Candle candle2 = new Candle("Fragrant Floral's", 5, 7, 18.75);
-        Candle candle3 = new Candle("Spicy N' Dicey", 2, 12, 11.98);
+        Candle lavender = new Candle("Lavender", 3, 5, 7.50);
+        Candle floral = new Candle("Fragrant Floral's", 5, 7, 18.75);
+        Candle cinna = new Candle("Spicy Cinnaimon", 2, 12, 11.98);
 
         // Calculating totals
-        int totalBurnTime = (int) (candle1.burnTime * candle1.quantity +
-                candle2.burnTime * candle2.quantity +
-                candle3.burnTime * candle3.quantity);
-        double totalDollarPerBurnTime = (candle1.price + candle2.price + candle3.price) / totalBurnTime;
-        double totalPrice = candle1.getSubtotal() + candle2.getSubtotal() + candle3.getSubtotal();
+        int totalBurnTime = (int) (lavender.burnTime * lavender.quantity +
+                floral.burnTime * floral.quantity +
+                cinna.burnTime * cinna.quantity);
+        double totalDollarPerBurnTime = (lavender.price + floral.price + cinna.price) / totalBurnTime;
+        double totalPrice = lavender.getSubtotal() + floral.getSubtotal() + cinna.getSubtotal();
 
         // Formatting totalDollarPerBurnTime to 2 decimal places
         DecimalFormat df = new DecimalFormat("#.##");
@@ -41,23 +41,23 @@ public class Main {
         // Generating the receipt
         System.out.println("Receipt\n");
         System.out.println("+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++");
-        System.out.println(candle1.name);
-        System.out.println("Amount: " + candle1.quantity);
-        System.out.println("Burn Time: " + candle1.burnTime + " hours");
-        System.out.println("Dollar per Burn Time: $" + df.format(candle1.dollarPerBurnTime));
-        System.out.println("Price: $" + candle1.price);
+        System.out.println(lavender.name);
+        System.out.println("Amount: " + lavender.quantity);
+        System.out.println("Burn Time: " + lavender.burnTime + " hours");
+        System.out.println("Dollar per Burn Time: $" + df.format(lavender.dollarPerBurnTime));
+        System.out.println("Price: $" + lavender.price);
         System.out.println("+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++\n");
-        System.out.println(candle2.name);
-        System.out.println("Amount: " + candle2.quantity);
-        System.out.println("Burn Time: " + candle2.burnTime + " hours");
-        System.out.println("Dollar per Burn Time: $" + df.format(candle2.dollarPerBurnTime));
-        System.out.println("Price: $" + candle2.price);
+        System.out.println(floral.name);
+        System.out.println("Amount: " + floral.quantity);
+        System.out.println("Burn Time: " + floral.burnTime + " hours");
+        System.out.println("Dollar per Burn Time: $" + df.format(floral.dollarPerBurnTime));
+        System.out.println("Price: $" + floral.price);
         System.out.println("+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++\n");
-        System.out.println(candle3.name);
-        System.out.println("Amount: " + candle3.quantity);
-        System.out.println("Burn Time: " + candle3.burnTime + " hours");
-        System.out.println("Dollar per Burn Time: $" + df.format(candle3.dollarPerBurnTime));
-        System.out.println("Price: $" + candle3.price);
+        System.out.println(cinna.name);
+        System.out.println("Amount: " + cinna.quantity);
+        System.out.println("Burn Time: " + cinna.burnTime + " hours");
+        System.out.println("Dollar per Burn Time: $" + df.format(cinna.dollarPerBurnTime));
+        System.out.println("Price: $" + cinna.price);
         System.out.println("+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++\n");
         System.out.println("Total Burn Time: " + totalBurnTime);
         System.out.println("Total Dollar per Burn Time: $" + formattedTotalDollarPerBurnTime);
